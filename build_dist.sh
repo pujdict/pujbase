ed19fd68be44d04955e2664658d1114c0495ff3b
brangpd <brangpd@outlook.com>
2025-06-02 15:23:13 +0800

    添加词库的 protobuf 数据。

--- build_dist.sh
+++ build_dist.sh
@@ -4,4 +4,9 @@ bash generate_protobuf.sh
 python3 generate_entries_db.py
 ls -l dist/entries.pb
 python3 generate_accents_db.py
-ls -l dist/entries.pb
+ls -l dist/accents.pb
+python3 generate_phrases_db.py
+ls -l dist/phrases.pb
+
+cp -r puj dist
+cp LICENSE* dist
---
+++ generate_phrases_db.py
@@ -0,0 +1,31 @@
+import csv
+from puj.phrases_pb2 import *
+from pathlib import Path
+
+
+def main():
+    phrases_dir = Path('phrases')
+    assert phrases_dir.exists() and phrases_dir.is_dir()
+    phrases = Phrases()
+    for phrase_file in phrases_dir.glob('*.csv'):
+        with open(phrase_file, 'r', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            rows = list(reader)
+        assert len(rows) > 1
+        for row in rows[1:]:
+            phrase = Phrase(
+                teochew=row[0],
+                puj=row[1],
+                mandarin=row[2] or None,
+                node=row[3] or None,
+            )
+            phrases.phrases.append(phrase)
+    with open('dist/phrases.pb', 'wb') as f:
+        f.write(phrases.SerializeToString())
+    with open('dist/phrases.pb', 'rb') as f:
+        phrases = Phrases()
+        phrases.ParseFromString(f.read())
+
+
+if __name__ == '__main__':
+    main()
