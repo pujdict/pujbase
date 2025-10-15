88573b80c74a81f844edfab5380b3101b1c0fc1c
brangpd <brangpd@outlook.com>
2025-10-15 22:23:01 +0800

    重构项目结构 src -> libpuj。

--- build_dist.sh
+++ build_dist.sh
@@ -1,15 +1,15 @@
 set -e
 
 bash generate_protobuf.sh
-pushd src
+pushd libpuj
   python3 generate_db.py
 popd
 ls -l dist/entries.pb
 ls -l dist/accents.pb
 ls -l dist/phrases.pb
 
-rm -rf src/__pycache__
+rm -rf libpuj/__pycache__
 
-mkdir -p dist/src
-cp -r src/*.py src/*.proto dist/src
+mkdir -p dist/libpuj
+cp -r libpuj/*.py libpuj/*.proto dist/libpuj
 cp LICENSE* dist
--- generate_protobuf.sh
+++ generate_protobuf.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-pushd src
+pushd libpuj
   protoc accents.proto --python_out=. --pyi_out=.
   protoc entries.proto --python_out=. --pyi_out=.
   protoc phrases.proto --python_out=. --pyi_out=.
