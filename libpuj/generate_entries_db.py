b19612ef2c5ad21e236acc772116553182e49d25
brangpd <brangpd@outlook.com>
2026-01-29 21:26:52 +0800

    口音 pb 数据中添加描述信息 title & note。

--- libpuj/generate_entries_db.py
+++ libpuj/generate_entries_db.py
@@ -128,6 +128,8 @@ def main():
         fuzzy_rule_eg = fuzzy_rule_details['eg']
         fuzzy_rule_desc = fuzzy_rule_details['desc']
         fuzzy_rule_actions = fuzzy_rule_details['act']
+        fuzzy_rule_title = fuzzy_rule_details['title']
+        fuzzy_rule_note = fuzzy_rule_details.get('note', '')
         for fuzzy_rule_action in fuzzy_rule_actions:
             action, pattern, replacement_dollar, _ = fuzzy_rule_action.split('/')
             replacement_backslash = re.sub(r'\$(\d)', r'\\\1', replacement_dollar)
@@ -145,6 +147,8 @@ def main():
             actions=actions,
             desc=fuzzy_rule_desc,
             examples=fuzzy_rule_eg,
+            title=fuzzy_rule_title,
+            note=fuzzy_rule_note,
         ))
 
     accents_file = Path('../data/accents.yml')
