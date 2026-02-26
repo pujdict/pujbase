f71e9845b463dcb8b88df267194d1dce0b162918
brangpd <brangpd@outlook.com>
2026-02-26 23:22:36 +0800

    generate_db 文件 import 补充 libpuj 前缀。

--- libpuj/generate_db.py
+++ libpuj/generate_db.py
@@ -1,10 +1,10 @@
-import generate_entries_db
-import generate_phrases_db
+import libpuj.generate_entries_db
+import libpuj.generate_phrases_db
 
 
 def main():
-    generate_entries_db.main()
-    generate_phrases_db.main()
+    libpuj.generate_entries_db.main()
+    libpuj.generate_phrases_db.main()
 
 
 if __name__ == '__main__':
